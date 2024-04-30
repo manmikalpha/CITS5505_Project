d54547bca59593a4516899354a69cb0e959031af
PavanPotukuchik <166984690+PavanPotukuchik@users.noreply.github.com>
2024-04-30 08:34:29 -0400

    Add files via upload

--- Flask/myapp.py
+++ Flask/myapp.py
@@ -25,8 +25,9 @@ def registration_redirect():
 def reg_redirect():
     return render_template('registration.html')
 
+
 @app.route('/authenticate', methods = ['GET', 'POST'])
-def get_user():
+def authenticate_user():
     if request.method == 'POST':
         login_user = (
             request.form['email'],
@@ -36,14 +37,16 @@ def get_user():
         user = auth_user(login_user)
 
         if user == None:
-            return render_template('login_failed.html')
+            flash("User not found. Please register to gain access.")
+            return render_template('login.html')
         
         elif user[3] == request.form['email']:
             return render_template('home.html')
         
         else:
-            return render_template('login_failed.html')
-        
+            flash("User not found. Please register to gain access.")
+            return render_template('login.html')      
+
 
 @app.route('/add', methods = ['GET', 'POST'])
 def add_user():     
@@ -54,14 +57,30 @@ def add_user():
             request.form['email'],
             request.form['pswd1']
             )
-    
+        
+    useremail = get_user(request.form['email'])
+
     if request.form['pswd1'] == request.form['pswd2']:
-        register_user (user_details)
-        return render_template('registration_success.html')
+        if useremail != request.form['email']:
+            register_user (user_details)
+            flash("Registration successful. Please log in")
+            return render_template('registration.html')
+        else:
+            flash("Email already exists. Please log in")
+            return render_template('registration.html')
     else:
         flash("Passwords do not match.")
         return render_template('registration.html')
 
+
+
+
+
+
+
+
+
+#Method 1 Authenticate User with Useremail and Password
 def auth_user(login_user):
     connection = sqlite3.connect(db_locale)
 
@@ -72,16 +91,25 @@ def auth_user(login_user):
     user = cursor.fetchone()
     return user   
 
+#Method 2 Register New user with Firstname, Lastname, email and Password
 def register_user(user_details):
     connection = sqlite3.connect(db_locale)
-
+    
     cursor = connection.cursor()
     sqlString = 'INSERT INTO users (user_fName, user_lName, user_email, user_pswd) VALUES (?, ?, ?, ?)'
         
     cursor.execute(sqlString, user_details)
-
     connection.commit()
     connection.close()
+
+#Method 3 Chceck user with email - if email already exists or not for registration
+def get_user(email):
+    connection = sqlite3.connect(db_locale)
+
+    cursor = connection.cursor()   
+    cursor.execute("SELECT * FROM users WHERE user_email = '" + email + "'")
+    user = cursor.fetchone()
+    return user[3]  
     
 if __name__ == "__main__":
     app.run()

9d059f1e73a6bf385a5c6efaf889766dbdb58b09
manmikalpha <mihirtayshete@gmail.com>
2024-05-17 16:17:25 +0800

    Restructured the whole code and added the ability to edit and delete events with also deleting the images stored in static folder

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
+from flask import Flask
+from flask_mail import Mail
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mykey'
+app.config['MAIL_SERVER']= "smtp.gmail.com"
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = "pythonuserflask@gmail.com"
+app.config['MAIL_PASSWORD'] =  "gurs szsm zbrh cljb"
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
-
+from . import routes
--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask
-from models import db,Events,Images
-from config import Config
-from datetime import datetime
+from .models import db
+from .config import Config
+from flask_mail import Mail
+from .routes import *
+from . import app
 
-app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mykey'
 app.config.from_object(Config)
 db.init_app(app)
 app.app_context().push()
 db.create_all()
 # db.drop_all( )
+
+#Email Configuration
+mail = Mail(app)
+app.config['MAIL_SERVER']= "smtp.gmail.com"# "live.smtp.mailtrap.io"
+app.config['MAIL_PORT'] = 465 #587
+app.config['MAIL_USERNAME'] = "pythonuserflask@gmail.com"#"api"
+app.config['MAIL_PASSWORD'] =  "gurs szsm zbrh cljb" #"0469d4f1cf2bb55c5ee45c43e50321c9"
+#app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 

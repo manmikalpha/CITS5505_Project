241eedd8db5f0a3a9bc5dcfe5a8e06c299198c8a
ruthfan1991 <49708377+ruthfan1991@users.noreply.github.com>
2024-05-18 12:16:05 +0800

    try to write a test, imitate from youtube

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from project import create_app, db
+
+@pytest.fixture()
+def app():
+    app = create_app("sqlite:///")
+    with app.app_context():
+        db.create_all()
+
+    print("CREATING DATABASE")
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
